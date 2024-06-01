b953809cce220cb471364f0f91af1293cb84cebd
shradha mantry <itsmehak27@gmail.com>
2024-06-02 01:22:36 +0530

    final-changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        // Specify the entry JavaScript file for your application
+        main: 'src/main.jsx' // Adjust the path as needed
+      }
+    }
+  }
+});
