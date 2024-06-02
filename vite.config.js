62b58ee991e8b75635813f1941a4c1f765ba734a
shradha mantry <itsmehak27@gmail.com>
2024-06-02 14:24:45 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
   build: {
-    outDir: 'build',
+    outDir: 'dist', // Ensure the build output is in the 'dist' directory
     rollupOptions: {
       input: {
-        main: 'src/main.jsx' 
+        main: 'public/index.html' // This should point to the index.html file in the public directory
       }
     }
+  },
+  server: {
+    open: true, // Automatically open the app in the browser
   }
 });
