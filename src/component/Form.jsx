c13781e01cd190d73ae9a35944d63b474d55d817
shradha mantry <itsmehak27@gmail.com>
2024-06-03 22:58:16 +0530

    commit

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -17,7 +17,7 @@ const Form = () => {
           <div className='form3'>
             <label className='block text mt-2'>*Enter your Whatsapp Number</label>
             <div className="flex">
-              <select className="box2">
+              <select className="box2 choose1">
                 <option value="+91">+91</option>
               </select>
               <input type="text" placeholder="Eg. 0000000000" className="box" />
@@ -25,21 +25,21 @@ const Form = () => {
           </div>
           <div className='form3'>
             <label className="block text">*Select your profession</label>
-            <select className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none">
+            <select className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none choose1">
               <option value="">Choose the most relevant option</option>
             </select>
           </div>
 
           <div className='form3'>
             <label className="block text">*Select your goal</label>
-            <select className="w-full py-2 mt-2 border rounded-md focus:outline-none">
-              <option value="Choose" >Choose the most relevant option</option>
+            <select className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none choose1 ">
+              <option value="Choose">Choose the most relevant option</option>
             </select>
           </div>
 
           <div className='form3'>
             <label className="block text">*Select your city</label>
-            <select className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none">
+            <select className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none choose1">
               <option value="Choose">Choose the most relevant option</option>
             </select>
           </div>
